a17e16bb6b37b1c6ce8b242168304f8a863034c2
Alex Villecco <amvillecco@teco.com.ar>
2021-11-24 09:09:46 -0300

    cambiamos el color de PokeThumbnail dependiendo del type del pokemon

--- src/components/PokeThumbnail.js
+++ src/components/PokeThumbnail.js
@@ -1,14 +1,18 @@
-const PokeThumbnail = ({ id, name, image, type }) => (
-  <div className="thumb-container">
-    <div className="number">
-      <small>#{id}</small>
+const PokeThumbnail = ({ id, name, image, type }) => {
+  const thubmStyle = `thumb-container ${type}`
+
+  return(
+    <div className={thubmStyle}>
+      <div className="number">
+        <small>#{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div className="detail-wrapper">
+        <h3>{name}</h3>
+        <small>Type: {type}</small>
+      </div>
     </div>
-    <img src={image} alt={name} />
-    <div className="detail-wrapper">
-      <h3>{name}</h3>
-      <small>Type: {type}</small>
-    </div>
-  </div>
-)
+  )
+}
 
 export default PokeThumbnail;
