f20aa35d53fb627cd9ed6732836e5ff3bdc9ee39
Alex Villecco <amvillecco@teco.com.ar>
2021-11-24 09:07:12 -0300

    renderizo cada pokemon en su PokeThumbnail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import PokeThumbnail from './components/PokeThumbnail';
 
 const App = () => {
   const [allPokemons, setAllPokemons] = useState([]);
@@ -30,7 +31,19 @@ const App = () => {
     <div className="app-container">
       <h1 className="title">Poke React App</h1>
       <div className="pokemon-container">
-        {allPokemons.map(pokemon => <li>{pokemon.name}</li>)}
+        <div className="all-container">
+          {allPokemons.map(
+            (pokemon, index) =>
+              <PokeThumbnail
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+                key={index}
+              />
+            )
+          }
+        </div>
       </div>
       <button className="load-more">cargar más</button>
     </div>
