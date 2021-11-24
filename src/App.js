40359cb391fc0a6efcb44d6ddef6ba34c475b665
Alex Villecco <amvillecco@teco.com.ar>
2021-11-24 08:34:43 -0300

    hacemos la consulta a los primeros 20 pokemon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const getAllPokemons = async () => {
+    const response = await fetch(loadMore);
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
     <div className="app-container">
       <h1 className="title">Poke React App</h1>
